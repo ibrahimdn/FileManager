3945014ce2b74ff6f265b334c416cb9c89e50f5d
ibrahimdn <ibrahimdn@ibrahimdn-Mac.local>
2017-12-31 04:07:19 +0300

    Initial Commit

---
+++ filemanager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  filemanager
+//
+//  Created by ibrahimdn on 31.12.2017.
+//  Copyright © 2017 ibrahimdn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
